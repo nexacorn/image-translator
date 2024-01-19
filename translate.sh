18c133921b316ff373ae16ffd3ec08772df5ce6e
Ubuntu <ubuntu@ip-172-31-3-142.ap-northeast-2.compute.internal>
2024-01-19 04:13:34 +0000

    adding language option

--- translate.sh
+++ translate.sh
@@ -1,3 +1,4 @@
 export OPENAI_API_KEY="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"
-LOG_NAME=sample_vietnam
-python3 image_translate.py indir=$(pwd)/input_images outdir=$(pwd)/output/$LOG_NAME font=$(pwd)/font/NotoSerifCJKsc-VF.ttf
+LOG_NAME=sample_korea
+LANGUAGE=ko
+python3 image_translate.py indir=$(pwd)/input_images outdir=$(pwd)/output/$LOG_NAME font=$(pwd)/font/NotoSerifCJKsc-VF.ttf lang=$LANGUAGE
