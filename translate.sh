4f39261b7192e7d3bdc31784c4c545afa1856f57
Ubuntu <ubuntu@ip-172-31-3-142.ap-northeast-2.compute.internal>
2024-01-19 08:54:17 +0000

    edit translate.sh

--- translate.sh
+++ translate.sh
@@ -1,4 +1,5 @@
-export OPENAI_API_KEY="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"
+export OPENAI_API_KEY=""
+input_folder=input_images
 LOG_NAME=sample_vietnam_font_cluster
 LANGUAGE=vi
-python3 image_translate.py indir=$(pwd)/input_images outdir=$(pwd)/output/$LOG_NAME font=$(pwd)/font/NotoSerifCJKsc-VF.ttf lang=$LANGUAGE
+python3 image_translate.py indir=$(pwd)/$input_folder outdir=$(pwd)/output/$LOG_NAME font=$(pwd)/font/NotoSerifCJKsc-VF.ttf lang=$LANGUAGE
