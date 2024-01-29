618f0b171b0511b3cbee5b3e958d1ccdfd1c28bd
Ahn-Hyun <presentahn@showniq.ai>
2024-01-29 09:46:24 +0000

    use only flask

--- flask_translate.py
+++ flask_translate.py
@@ -1,14 +1,65 @@
 from flask import Flask, request, jsonify
-from celery_translate import translate_image
-import multiprocessing
+from module.translate_module import process_image, upload_image_url
+import requests
+from io import BytesIO
+from PIL import Image, ImageDraw, ImageFont
+from lama_cleaner.model_manager import ModelManager
+from paddleocr import PaddleOCR
+from urllib.parse import urlparse, unquote
+from dotenv import load_dotenv
+import os
 
-multiprocessing.set_start_method("spawn", force=True)
+load_dotenv()
+
 app = Flask(__name__)  # Flask 애플리케이션 인스턴스 생성
+
+global_lama = None
+global_ocr = None
+model: ModelManager = None
+
+def load_models():
+    global global_lama, global_ocr
+    if global_lama is None or global_ocr is None:
+        global_lama = ModelManager(name='lama', device='cuda')
+        global_ocr = PaddleOCR(lang="ch", show_log=False, use_gpu=True, ocr_version="PP-OCRv3")
+    return global_lama, global_ocr
+
+def extract_filename_from_url(url):
+    parsed_url = urlparse(url)
+    filename = parsed_url.path.split('/')[-1]
+    decoded_filename = unquote(filename)
+    return decoded_filename
+
+model, ocr = load_models()
 
 @app.route('/process-images', methods=['POST'])
 def process_images():
-    print(request.json)
     image_urls = request.json.get('urls', [])
-    print(image_urls)
-    task = translate_image.delay(image_urls)
-    return jsonify({"task_id": task.id})
+    print("image urls : ", image_urls)
+    
+    processed_urls = []
+    openai_api_key = os.getenv('OPENAI_KEY')
+    
+    for url in image_urls:
+        file_name = extract_filename_from_url(url)
+        
+        response = requests.get(url)
+        
+        if response.status_code == 200:
+            image_data = BytesIO(response.content)
+
+            image = Image.open(image_data)
+        else:
+            print(f"Failed to retrieve image. Status code: {response.status_code}")
+            return
+        
+        font = './font/NanumGothicBold.ttf'
+        to_lang = 'ko'
+            
+        processed_image = process_image(image, font, to_lang, model, ocr, file_name, openai_api_key)
+        print(processed_image)
+        processed_url = upload_image_url(processed_image, file_name)
+        
+        processed_urls.append(processed_url)
+    
+    return processed_urls
