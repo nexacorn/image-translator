368fae77cffa6a966328e00910849e0b8e29a110
Ahn-Hyun <presentahn@showniq.ai>
2024-01-30 05:40:49 +0000

    organize

--- setting.sh
+++ setting.sh
@@ -1,8 +1,9 @@
 pip3 install -r requirements.txt
 python -m pip install paddlepaddle -i https://pypi.tuna.tsinghua.edu.cn/simple
 pip install "paddleocr>=2.0.1" 
-#GPU
-#conda install pytorch torchvision torchaudio cudatoolkit=10.2 -c pytorch -y
-conda install pytorch torchvision torchaudio -c pytorch -y
+# GPU
+conda install pytorch torchvision torchaudio cudatoolkit=10.2 -c pytorch -y
+# CPU
+# conda install pytorch torchvision torchaudio -c pytorch -y
 pip install pytorch-lightning==1.2.9
 pip install Pillow==9.5.0
